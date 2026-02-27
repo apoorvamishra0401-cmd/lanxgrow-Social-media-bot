abab744e79ecdb85d55c8b28d78436eb00962e64
apoorvamishra0401-cmd <apoorvamishra0401@gmail.com>
2026-02-27 23:26:02 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import os
+import json
+import time
+import requests
+from datetime import datetime
+import fal_client
+
+# ── Keys ──────────────────────────────────────────────────────
+GROQ_API_KEY = "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"
+TELEGRAM_TOKEN = "8733495512
